abf0b9822371a2b34fc57edfdb8f064548266d5c
Mamzin Evgeny <evgeny.mamzin@lamoda.ru>
2017-02-28 09:18:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert(1)
+document.write('<span id="preloader">Ждем обновлений...</span>');
+
+        if (!$.ajax) {
+            var script = document.createElement('script');
+            script.src = 'https://code.jquery.com/jquery-1.11.0.min.js';
+            script.type = 'text/javascript';
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
